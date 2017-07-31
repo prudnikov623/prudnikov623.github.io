ba6741dd3debbf3a5f90991bcdcec7e75dcdb023
Alexandrgm23 <prudnikov623@gmail.ru>
2017-07-31 23:24:35 +0300

    commit12
    asd

--- test5/js/timer.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-	jQuery(".eTimer").eTimer({
-		etType: 1, etDate: "15.06.2017.0.0", etTitleText: "", etTitleSize: 20, etShowSign: 0, etSep: " ", etFontFamily: "Trebuchet MS", etTextColor: "#a3a3a3", etPaddingTB: 15, etPaddingLR: 15, etBackground: "transparent", etBorderSize: 0, etBorderRadius: 2, etBorderColor: "transparent", etShadow: " 0px 0px 10px 0px transparent", etLastUnit: 4, etNumberFontFamily: "FranklinGothicDemiCond", etNumberSize: 60, etNumberColor: "white", etNumberPaddingTB: 0, etNumberPaddingLR: 0, etNumberBackground: "transparent", etNumberBorderSize: 0, etNumberBorderRadius: 5, etNumberBorderColor: "transparent", etNumberShadow: "inset 0px 0px 10px 0px transparent"
-	});
-	jQuery(".eTimer1").eTimer({
-		etType: 1, etDate: "15.06.2017.0.0", etTitleText: "", etTitleSize: 20, etShowSign: 0, etSep: " ", etFontFamily: "Trebuchet MS", etTextColor: "#a3a3a3", etPaddingTB: 15, etPaddingLR: 15, etBackground: "transparent", etBorderSize: 0, etBorderRadius: 2, etBorderColor: "transparent", etShadow: " 0px 0px 10px 0px transparent", etLastUnit: 4, etNumberFontFamily: "FranklinGothicDemiCond", etNumberSize: 90, etNumberColor: "white", etNumberPaddingTB: 0, etNumberPaddingLR: 8, etNumberBackground: "transparent", etNumberBorderSize: 0, etNumberBorderRadius: 5, etNumberBorderColor: "transparent", etNumberShadow: "inset 0px 0px 10px 0px transparent"
-	});
-});
